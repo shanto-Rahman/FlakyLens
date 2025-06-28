49924d081fbcaa4a4cca112665c0bebcde7d2fdf
SHANTO <shanto.rahman@utexas.edu>
2025-06-27 23:27:21 -0500

    rq1

---
+++ src/rq1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+currentDir=$(pwd)
+bash per_project_prediction.sh FlakyLens "BERT"
+cd ../results/scripts
+bash per_category_parse_result.sh ../per_Category_Evaluation_BERT-FlakyLens.txt
+cd $currentDir
